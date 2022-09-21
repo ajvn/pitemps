5abb88b5d798e59cacc07ab47b5041172ceb2299
Ivan <ivans@vaskir.co>
2022-09-21 19:31:49 +0200

    update: Updating script a bit
    Signed-off-by: Ivan <ivans@vaskir.co>

--- pitemps.sh
+++ pitemps.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -Eeou pipefail
 
-gpu=$(sudo vcgencmd measure_temp | awk -F"=" '{print $2}')
-initial_cpu=$(cat /sys/class/thermal/thermal_zone0/temp)
-cpu=$((initial_cpu/1000))
+# vcgencmd is available in libraspberrypi-bin package in
+# Ubuntu 22.04
+gpu="$(sudo vcgencmd measure_temp | awk -F"=" '{print $2}' | cut -d\' -f1)"
+initial_cpu="$(cat /sys/class/thermal/thermal_zone0/temp)"
+cpu="$(echo "scale=1; $initial_cpu/1000" | bc)"
 
-echo "GPU Temp ==> $gpu"
-echo "CPU Temp ==> $cpu'C"
+echo -e "GPU Temp: $gpu \u2103"
+echo -e "CPU Temp: $cpu \u2103"
